aabe69364c518cb4b3646651f497f13aa52d18e6
Hafiz Muhammad Umar Rafique <hmurafique.career.93@gmail.com>
2026-04-21 15:49:18 +0500

    Remove sensitive API key from server.js
    Removed sensitive API key from server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const open_api_key = "shimasd123";  
+
 app.get('/', (req, res) => {
   res.send('Hello from my custom Docker deployment!');
 });
